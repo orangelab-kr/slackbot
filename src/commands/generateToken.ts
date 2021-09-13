29333d8c5d731f4ca838430dd048af716604f71c
bakjonghun <bakjonghun@hikick.kr>
2021-09-13 15:56:29 +0900

    change: get token with stage in generateToken

--- src/commands/generateToken.ts
+++ src/commands/generateToken.ts
@@ -21,35 +21,76 @@ const services: { [key: string]: any } = {
   'openapi-webhook': InternalWebhookClient,
 };
 
-const secretKeys: { [key: string]: any } = {
-  'openapi-discount': process.env.HIKICK_OPENAPI_DISCOUNT_KEY,
-  'openapi-franchise': process.env.HIKICK_OPENAPI_FRANCHISE_KEY,
-  'openapi-insurance': process.env.HIKICK_OPENAPI_INSURANCE_KEY,
-  'openapi-kickboard': process.env.HIKICK_OPENAPI_KICKBOARD_KEY,
-  'openapi-location': process.env.HIKICK_OPENAPI_LOCATION_KEY,
-  'openapi-platform': process.env.HIKICK_OPENAPI_PLATFORM_KEY,
-  'openapi-ride': process.env.HIKICK_OPENAPI_RIDE_KEY,
-  'openapi-webhook': process.env.HIKICK_OPENAPI_WEBHOOK_KEY,
+const issuers: { stg?: string; prd?: string } = {
+  stg: process.env.HIKICK_OPENAPI_STG_ISSUER,
+  prd: process.env.HIKICK_OPENAPI_PRD_ISSUER,
+};
+
+const secretKeys: {
+  stg: { [key: string]: any };
+  prd: { [key: string]: any };
+} = {
+  stg: {
+    'openapi-discount': process.env.HIKICK_OPENAPI_STG_DISCOUNT_KEY,
+    'openapi-franchise': process.env.HIKICK_OPENAPI_STG_FRANCHISE_KEY,
+    'openapi-insurance': process.env.HIKICK_OPENAPI_STG_INSURANCE_KEY,
+    'openapi-kickboard': process.env.HIKICK_OPENAPI_STG_KICKBOARD_KEY,
+    'openapi-location': process.env.HIKICK_OPENAPI_STG_LOCATION_KEY,
+    'openapi-platform': process.env.HIKICK_OPENAPI_STG_PLATFORM_KEY,
+    'openapi-ride': process.env.HIKICK_OPENAPI_STG_RIDE_KEY,
+    'openapi-webhook': process.env.HIKICK_OPENAPI_STG_WEBHOOK_KEY,
+  },
+  prd: {
+    'openapi-discount': process.env.HIKICK_OPENAPI_PRD_DISCOUNT_KEY,
+    'openapi-franchise': process.env.HIKICK_OPENAPI_PRD_FRANCHISE_KEY,
+    'openapi-insurance': process.env.HIKICK_OPENAPI_PRD_INSURANCE_KEY,
+    'openapi-kickboard': process.env.HIKICK_OPENAPI_PRD_KICKBOARD_KEY,
+    'openapi-location': process.env.HIKICK_OPENAPI_PRD_LOCATION_KEY,
+    'openapi-platform': process.env.HIKICK_OPENAPI_PRD_PLATFORM_KEY,
+    'openapi-ride': process.env.HIKICK_OPENAPI_PRD_RIDE_KEY,
+    'openapi-webhook': process.env.HIKICK_OPENAPI_PRD_WEBHOOK_KEY,
+  },
 };
 
 export const getGenerateTokenCommand: Middleware<SlackCommandMiddlewareArgs> =
   async (ctx) => {
     await ctx.ack();
-    const { text } = ctx.body;
-    if (!Object.keys(services).includes(text)) {
+    const args = ctx.body.text.split(' ');
+    if (args.length < 2) {
       await ctx.say(
-        `다음 중 하나만 입력할 수 있습니다. ${Object.keys(services).join(', ')}`
+        '올바른 명령어를 입력해주세요. /generate-token <스테이지> <서비스명>'
       );
 
       return;
     }
 
-    const { user } = await ctx.client.users.info({ user: ctx.command.user_id });
-    const service = new services[text]({
-      issuer: String(process.env.HIKICK_OPENAPI_ISSUER),
-      email: user?.profile?.email || 'system@hikick.kr',
-      secretKey: secretKeys[text],
+    const serviceNames = Object.keys(services);
+    if (!serviceNames.includes(args[0])) {
+      await ctx.say(
+        `<서비스명>은 다음 중 하나만 입력할 수 있습니다. ${serviceNames.join(
+          ', '
+        )}`
+      );
+
+      return;
+    }
+
+    const stages = Object.keys(secretKeys);
+    if (!Object.keys(secretKeys).includes(args[1])) {
+      await ctx.say(
+        `<스테이지>는 다음 중 하나만 입력할 수 있습니다. ${stages.join(', ')}`
+      );
+    }
+
+    const [serviceName, stage]: [string, 'stg' | 'prd'] = <any>args;
+    const { user } = await ctx.client.users.info({
+      user: ctx.command.user_id,
     });
+
+    const issuer = issuers[stage];
+    const email = user?.profile?.email || 'system@hikick.kr';
+    const secretKey = secretKeys[stage][serviceName];
+    const service = new services[serviceName]({ issuer, email, secretKey });
 
     try {
       await ctx.say(service.getAccessKey());
